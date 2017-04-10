3dca6f2c0a837190c1f3ad7c859f468648816de0
Magnus Wahlstrand <magnus.wahlstrand@ericsson.com>
2017-04-10 18:07:24 +0200

    Spell 'Improbability' correctly in cs version and 'Caffeine' in py version

--- namegen.py
+++ namegen.py
@@ -20,7 +20,7 @@ def generateJapaneseishName():
 	return s
 
 def generateRandomName(markovChains, gadgetType = None):
-	buzzwords = ['Quantum', 'Dynamic', 'Static', 'Entangled', 'Nano', 'Relativistic', 'Mega', 'Super', 'Celestial', 'Ridgid', 'Negative', 'Frictionless', 'Superconducting', 'Particle', 'Nuclear', 'Evolutionary', 'Ultra', 'Hyper-G', 'Low-Gravity', 'Interstellar', 'Viscous', 'Inviscous', 'Escalating','Timeless','Hyper', 'Dangerous','Sparkly', 'Boiling', 'Bending', 'Torsion', 'Non-linear', 'Linear', 'Parabolic', 'Hyperbolic', 'Elliptical', 'Conductive', 'Cyclical', 'Sustainable', 'Caffene-free', 'Deep', 'Improbability', 'Infinite']
+	buzzwords = ['Quantum', 'Dynamic', 'Static', 'Entangled', 'Nano', 'Relativistic', 'Mega', 'Super', 'Celestial', 'Ridgid', 'Negative', 'Frictionless', 'Superconducting', 'Particle', 'Nuclear', 'Evolutionary', 'Ultra', 'Hyper-G', 'Low-Gravity', 'Interstellar', 'Viscous', 'Inviscous', 'Escalating','Timeless','Hyper', 'Dangerous','Sparkly', 'Boiling', 'Bending', 'Torsion', 'Non-linear', 'Linear', 'Parabolic', 'Hyperbolic', 'Elliptical', 'Conductive', 'Cyclical', 'Sustainable', 'Caffeine-free', 'Deep', 'Improbability', 'Infinite']
 	if gadgetType==None:
 		gadget = ['Gyro', 'Thruster', 'Rocket']
 	else:
